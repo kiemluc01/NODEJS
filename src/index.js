e3f3491512c300d0c52455ac1caba682613b173f
kiemluc01 <kiemluc01@gmail.com>
2022-10-18 18:57:40 +0700

    fixx bug connect DB

--- src/index.js
+++ src/index.js
@@ -1,42 +1,46 @@
 const path = require('path')
 const express = require('express')
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
 const morgan = require('morgan')
+const db = require('./config/DB')
+
+//connect
+db.connect()
 
 const app = express()
 const port = 8000
+const host = "127.0.0.1"
+
+const routes = require('./routes')
 
 //middleware
-app.use(express.urlencoded({
-    extended: true
-}))
-app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 // set url/file static
-app.use(express.static(__dirname + '/resources/public'))
-    // app.console.log(__dirname)
+app.use(express.static(__dirname + '/resources/public'));
+// app.console.log(__dirname)
 
 ///http logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 //template engine
-app.engine('hbs', handlebars.engine({
-    extname: '.hbs'
-}))
-app.set('view engine', 'hbs')
-app.set("views", path.join(__dirname, 'resources/views'))
+app.engine(
+    'hbs',
+    handlebars.engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-//route
-app.get('/', (req, res) => res.send('<h1>Nguyễn Kiêm Lực</h1>'))
-app.get('/Home', (req, res) => {
-    res.render('home')
-})
-app.post('/home', (req, res) => {
-    console.log(req.body)
-    res.send('xong!')
-})
-
+//ROUTE
+routes(app);
 
 app.listen(port, () => {
-    console.log(`Your app listening on port http://127.0.0.1:${port}`)
-})
+    console.log(`Your app listening on port http://${host}:${port}`);
+});
